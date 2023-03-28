ee0acc342643404ef17e90894abff4608ea3d9ee
tanisultana <tanisultana@gmail.com>
2023-03-27 22:00:46 -0500

    first commit

---
+++ src/main/java/com/collabaration/maven/COLLAB_MAVEN_PROJECT1/App.java
@@ -0,0 +1,13 @@
+package com.collabaration.maven.COLLAB_MAVEN_PROJECT1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
